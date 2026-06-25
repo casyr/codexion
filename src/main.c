13ac1b4a84764967f4be686ec3cc7511e00b78c3
Yann Riffard <yriffard@z1r7p1.42lyon.fr>
2026-06-25 15:07:25 +0200

    the monitoring routine is checking some burnout case since last_compile and this is protected by a mutex. The main is more hashed. I need to split moe thr main fct for the code to be more readable

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: yriffard <yriffard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/23 08:21:18 by yriffard          #+#    #+#             */
-/*   Updated: 2026/06/24 13:49:57 by yriffard         ###   ########.fr       */
+/*   Updated: 2026/06/25 15:07:01 by yriffard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,31 +43,122 @@ void	*routine()
 	printf("la\n");
 }
 
+long	ft_get_time()
+{
+	struct timeval	current_time;
+	long 			result;
+
+	if(gettimeofday(&current_time, NULL) != 0)
+		return (1);
+	result = (current_time.tv_sec * 1000) + (current_time.tv_usec / 1000);
+	return (result);
+}
+
 void	*monitoring_routine(void* monitor)
 {
 	int 			compiling_counter;
-	int 			time;
+	long 			time;
 	struct timeval	current_time;
 	t_monitoring 	monitoring;
-
-	compiling_counter = 0;
+	
+	monitoring = *(t_monitoring*)monitor;
 	while(1)
 	{
-		time = gettimeofday(&current_time, NULL);
-		if (time + monitoring.last_compile > time + monitoring.time_to_burnout)
+		time = ft_get_time();
+		if (time == 1)
+		{
+			printf("gettimeofday fail in monitoring rountine");
+			break;
+		}
+		pthread_mutex_lock(&(monitoring.mutex));
+		// printf("%ld - %ld = %ld > %d \n", time, monitoring.last_compile, time - monitoring.last_compile, monitoring.time_to_burnout);
+		if (time - monitoring.last_compile > monitoring.time_to_burnout)
+		{
 			printf("BURNOUT!");
-		printf("time: %d", time);
+			break;
+		}
+		pthread_mutex_unlock(&(monitoring.mutex));
 	}
 }
 
-// void	monitoring_init(t_monitoring *monitor, )
-// {
-// }
+int	monitoring_init(t_monitoring *monitor, pthread_mutex_t monitor_mutex,
+						char **argv, t_coder *coder_list)
+{
+	monitor->compiling_nb = atoi(argv[6]);
+	monitor->last_compile = ft_get_time();
+	monitor->mutex = monitor_mutex;
+	if (monitor->last_compile == 1)
+	{
+		printf("gettimeofday of last_compile fail");
+		return (1);
+	}
+	monitor->time_to_burnout = atoi(argv[2]);
+	return (0);
+}
 
 // void coder_init(t_coder *coder)
 // {
 	
 // }
+
+int	coder_thread_creation(t_coder *coder_list, int coders_nb, pthread_t *coder_th)
+{
+	t_coder		coder;
+	int			coder_index;
+
+	coder_index = 0;
+	while (coder_index < coders_nb)
+	{
+		coder.id = coder_index;
+		coder.state = "NOT FINISH";
+		if(pthread_create(&(coder_th[coder_index]), NULL, &routine, &coder) != 0)
+		{
+			printf("coder %i thread CREATION fails\n", coder_index);
+			return (1);
+		}
+		coder_list[coder_index].id = coder_index;
+		coder_list[coder_index].state = coder.state;
+		coder_index++;
+	}
+	return (0);
+}
+
+int	monitoring_thread_creation(t_monitoring monitor, pthread_t *monitoring_th)
+{
+	if (pthread_create(monitoring_th, NULL, &monitoring_routine, &monitor) != 0)
+	{
+		printf("monitoring thread CREATION fails");
+		return (1);
+	}
+	return (0);
+}
+
+int	coder_thread_join(int coders_nb, pthread_t *coder_th)
+{
+	int coder_index;
+
+	coder_index = 0;
+	while (coder_index < coders_nb)
+	{
+		if (pthread_join(coder_th[coder_index], NULL) != 0)
+		{
+			printf("coder %d thread JOIN fails\n", coder_index);
+			return (1);
+		}
+		coder_index++;
+	}
+	return(0);
+}
+
+int monitor_thread_join(pthread_t *monitoring_th)
+{
+	if (pthread_join(*monitoring_th, NULL) != 0)
+	{
+		printf("monitoring th-1read JOIN fails\n");
+		return (1);
+	}
+	return (0);
+}
 
 int		main(int argc, char **argv)
 {
@@ -77,16 +168,16 @@ int		main(int argc, char **argv)
 	int				compiling_nb;
 	pthread_t 		*coder_th;
 	pthread_t		monitoring_th;
+	pthread_mutex_t	monitor_mutex;
 	t_monitoring	monitor;
 	t_coder			coder;
 	struct timeval	current_time;
+	t_coder			*coder_list;
 
-	coder_index = 0;
 	parsed_data = parsing(argc, argv);
 	if (parsed_data != 0)
-		return parsed_data;
+		return (-1);
 	coders_nb = atoi(argv[1]);
-	compiling_nb = atoi(argv[6]);
 	if (coders_nb < 2)
 	{
 		printf("number of coders must be > 1");
@@ -103,37 +194,20 @@ int		main(int argc, char **argv)
 		printf("malloc fails");
 		return (1);
 	}
-	monitor.compiling_nb = compiling_nb;
-	monitor.last_compile = gettimeofday(&current_time, NULL);
-	while (coder_index < coders_nb)
-	{
-		coder.id = coder_index;
-		coder.state = "NOT FINISH";
-		if(pthread_create(&(coder_th[coder_index]), NULL, &routine, &coder) != 0)
-		{
-			printf("coder %i thread CREATION fails\n", coder_index);
-			return (1);
-		}
-		coder_index++;
-	}
-	if(pthread_create(&monitoring_th, NULL, &monitoring_routine, &monitor) != 0)
-	{
-		printf("monitoring thread CREATION fails");
+	coder_list = malloc(sizeof(coder) * coders_nb);
+	if(coder_thread_creation(coder_list, coders_nb, coder_th) != 0)
 		return (1);
-	}
-	while (coder_index < coders_nb)
-	{
-		if(pthread_join(coder_th[coder_index], NULL) != 0)
-		{
-			printf("coder %d thread JOIN fails\n", coder_index);
-			return (1);
-		}
-		coder_index++;
-	}
-	if(pthread_join(monitoring_th, NULL) != 0)
-	{
-		printf("monitoring thread JOIN fails\n");
-		return (1);
-	}
+	if(monitoring_init(&monitor, monitor_mutex, argv, coder_list) != 0)
+		return (2);
+	if (pthread_mutex_init(&monitor_mutex, NULL) !=0)
+		return (3);
+	if(monitoring_thread_creation(monitor, &monitoring_th) != 0)
+		return (4);
+	if (coder_thread_join(coders_nb, coder_th) != 0)
+		return (5);
+	if (monitor_thread_join(&monitoring_th) != 0)
+		return (6);
+	if (pthread_mutex_destroy(&monitor_mutex) != 0)
+		return (7);
 	return (0);
 }
--- src/monitoring.h
+++ src/monitoring.h
@@ -2,8 +2,9 @@
 
 typedef struct monitor
 {
-	int		last_compile;
-	int		time_to_burnout;
-	int		compiling_nb;
-	t_coder *coder_list;
+	long			last_compile;
+	long			time_to_burnout;
+	int				compiling_nb;
+	t_coder 		*coder_list;
+	pthread_mutex_t	mutex;
 } t_monitoring;
